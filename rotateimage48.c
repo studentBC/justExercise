cd6b5d8e04427353fe29e926702a0008b69a512b
benchin <ben.chin@quantatw.com>
2018-04-23 14:08:36 +0800

    completed ! but haven't put on website to test yet ...

--- rotateimage48.c
+++ rotateimage48.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 void rotate(int** matrix, int matrixRowSize, int* matrixColSizes) {
 //	matrix = malloc((*matrixColSizes)*sizeof(int*));
 //	int col;
@@ -10,52 +9,56 @@ void rotate(int** matrix, int matrixRowSize, int* matrixColSizes) {
 //https://www.geeksforgeeks.org/inplace-rotate-square-matrix-by-90-degrees/?qa-rewrite=4493/rotate-the-matrix-inplace
       int i= 0;
       int turns = matrixRowSize;
+	  int count = 0;
       //first we have to know the four actual location to rotate
-      int top , right,buttom,left,temp,startRow,endRow;
-      top = startRow = 0;left = right =buttom= endRow = matrixRowSize-1;
-
+      int lux,luy,rux,ruy,rdx,rdy,ldx,ldy,temp;
+	  lux = luy = rux = ldy = 0;rdx = rdy = ldx = ruy = matrixRowSize-1;
       while(turns>=2){
 
 		    // for 4 points to rotate at each round
-		  for(i=0:i < matrixRowSize;i++){
+		  for(i=0;i < ruy-count;i++){
 		  	//for the top to right
-		  	temp = matrix[startRow+i][right];
-		  	matrix[startRow+i][right] = matrix[startRow][top+i];
+		  	temp = matrix[rux+i][ruy];
+		  	matrix[rux+i][ruy] = matrix[lux][luy+i];
 		  	//for the left to top 
-		  	matrix[startRow][top+i] = matrix[endRow-i][left];
+		  	matrix[lux][luy+i] = matrix[ldx-i][ldy];
 		  	//for the buttom to left
-		  	matrix[endRow-i][left] = temp;
+		  	matrix[ldx-i][ldy] = matrix[rdx][rdy-i];
+			//for right to buttom
+			matrix[rdx][rdy-i] = temp;
 		  }
-		  startRow+=1;endRow-=1;
-		  top+=1;right-=1;buttom-=1;left-=1;
+		  lux+=1;luy+=1;rux+=1;ruy-=1;rdx-=1;rdy-=1;ldx-=1;ldy+=1;
           turns-=2;
+		  count++;
       }   
 	return ;	
 }
 int main(){
 //	int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
 //	int* matrixColSizes = &matrix;
-	int **matrix;
-	int matrixColSize = 3;
+	int **matrix = NULL;
+	int matrixColSize = 10;
 	int *matrixColSizes = &matrixColSize;
-	*matrix = matrixColSizes;
+	//printf("the matrixColSizes is %d",*matrixColSizes);
 	matrix = malloc(*matrixColSizes*sizeof(int*));
 	int col,row,count = 1;
-	for(col=0;col<3;col++){
-		matrix[col] = malloc(3*sizeof(int));
-		for(row=0;row<3;row++){
+	for(col=0;col<*matrixColSizes;col++){
+		matrix[col] = malloc(*matrixColSizes*sizeof(int));
+		for(row=0;row<*matrixColSizes;row++){
 			matrix[col][row] = count;
-			printf("%d",matrix[col][row]);
+			printf("%d ",matrix[col][row]);
 			count++;
 		}
 		printf("\n");
 	}
-	rotate(matrix,3,matrixColSizes);
+	//*matrix = matrixColSizes;
+	printf("-------------- \n");
+	rotate(matrix,matrixColSize,matrixColSizes);
 	int i = 0;
 	int j = 0;
-	for(i = 0;i< 3;i++){
-		for(j = 0;j<3;j++){
-			printf("%d",matrix[i][j]);
+	for(i = 0;i< matrixColSize;i++){
+		for(j = 0;j<matrixColSize;j++){
+			printf("%d ",matrix[i][j]);
 		}
 		printf("\n");
 	}
