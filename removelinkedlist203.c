4e244a370d5b2c47d5cbcb0892471696a8bb4b86
BenChin <ben.chin@quantatw.com>
2018-04-25 10:38:15 +0800

    start another practice now

---
+++ removelinkedlist203.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    
+}
