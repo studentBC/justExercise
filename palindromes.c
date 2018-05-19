4797c0bd6626636e251d671afc5e13aeafc4ae71
ben.chin <ben.chin@quantatw.com>
2018-05-19 21:56:39 +0800

    there is still some error need to be fix...

--- palindromes.c
+++ palindromes.c
@@ -1,59 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-char* longestPalindrome(char* s) {
-    int length = strlen(s);
-    char* Palindrome = NULL;
-    if (length == 1){
-        return s;
-    }
-    int i = 0;
-    int alphabet[26];
-    int location[length];
-    int count = 0;
-    for (i = 0;i< length;i++){
-        printf("%d\n",s[i]-97);
-        alphabet[s[i]-97]++;
-    }
-    char possible[length];
-    for (i =0;i<26;i++){
-        if (alphabet[i] >1){
-            count++;
-        }
-    }
-    int j = 0 , k = 0 ,start, from = 1000,to = -1;
-    int newlength = 0;char* newstr=NULL;
-    char* longestPalindrome = "";
-    while(start<26){
-        
-        if(alphabet[start] > 1){
-            for (j = 0; j < length;j++){
-                if ((s[j]-97) == start){
-                    if (j<from) {
-                        from = j;
-                    } else if (j > to){
-                        to = j;
-                    }
-                }
-            }
-            newlength = to-from+1;
-            newstr = malloc(sizeof(char)*(newlength+1));
-            for (i = 0; i < newlength;i++){
-                newstr[i] = s[from+i];
-            }
-            newstr = findPalindrome(newstr);
-            printf("%s",newstr);
-            if (strlen(newstr) > strlen(longestPalindrome)){
-                longestPalindrome = newstr;
-            }else{
-                free(newstr);
-            }
-        }
-        start++;
-    }
-     return Palindrome;
-
-}
+#include<stdbool.h>
 char* findPalindrome(char* s){
     int length = strlen(s);
    
@@ -74,11 +22,11 @@ char* findPalindrome(char* s){
       //find the longthest
       int start;
       for(i = 0; i < length;i++){
-          if(mark[i]){
+          if (mark[i]) {
               count++;
               printf("count %d",count);
-              if(count ==2 ){
-                  start = i-1;
+              if(count == 1) {
+                  start = i;
               }
               if (count>biggestCount) {
                       biggestCount = count;
@@ -94,7 +42,81 @@ char* findPalindrome(char* s){
       }
      return Palindrome;
 } 
+char* longestPalindrome(char* s) {
+    int length = strlen(s);
+    char* Palindrome = NULL;
+    if (length == 1){
+        return s;
+    }
+    int i = 0;
+    int alphabet[52]={0};
+    //int location[length];
+    int count = 0;
+    for (i = 0;i< length;i++){
+        if(s[i]<97){
+            alphabet[s[i]-65]++;   
+        }else{
+            alphabet[s[i]-71]++;
+        }
+    }
+/*
+    for (i =0;i<26;i++){
+        if (alphabet[i] >1){
+            count++;
+        }
+    }
+*/
+    int j = 0 , k = 0 ,start = 0, from = 1000,to = -1;
+    int newlength = 0;char* newstr=NULL;
+    char* longestPalindrome = "";
+    while(start<52){
+        
+        if(alphabet[start] > 1){
+            printf("start %d\n",start);
+            for (j = 0; j < length;j++){
+                if (s[j]<97){
+                    if ((s[j]-65) == start){
+                        if (j<from) {
+                            from = j;
+                        } else if (j > to){
+                            to = j;
+                        }
+                    }
+                }else{
+                    if ((s[j]-71) == start){
+                        printf("%d\n",start);
+                        if (j<from) {
+                            from = j;
+                        } else if (j > to){
+                            to = j;
+                        }
+                    }
+                }
+            }
+            printf(" from %d  to  %d \n",from,to);
+            newlength = to-from+1;
+            newstr = (char* )malloc(sizeof(char)*(newlength+1));
+            for (i = 0; i < newlength;i++){
+                newstr[i] = s[from+i];
+            }
+            printf("input is %s\n",newstr);
+            newstr = findPalindrome(newstr);
+            printf("%s",newstr);
+            if (strlen(newstr) > strlen(longestPalindrome)){
+                longestPalindrome = newstr;
+            }else{
+                free(newstr);
+            }
+        }
+        start++;
+    }
+     return longestPalindrome;
+
+}
+
+ 
 int main(){
 	char* palindrome= "spxps";
 	printf("%s\n",longestPalindrome(palindrome));
+	return 0;
 }
