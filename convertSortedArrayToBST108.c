5dd0033b61a0decb1945a29538ea7a08f2dba711
ben.chin <ben.chin@quantatw.com>
2018-07-07 20:41:18 +0800

    pass the test and is the fatest

--- convertSortedArrayToBST108.c
+++ convertSortedArrayToBST108.c
@@ -6,20 +6,43 @@
  *     struct TreeNode *right;
  * };
  */
-struct TreeNode* newNode(int data)
+ struct TreeNode * newNode(int data) {
+     struct TreeNode * node = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+     node->val = data;
+     node->left = node->right = NULL;
+     return (node);
+ }
+ void preOrder(struct TreeNode* node)
 {
-    struct TreeNode * node = malloc(sizeof(TreeNode));
-    node->val = data;
-    node->left = node->right = NULL;
-    return (node);
+    if (node == NULL)
+        return;
+    printf("%d ", node->val);
+    preOrder(node->left);
+    preOrder(node->right);
 }
-
-struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
-    int half = numsSize/2;
-    Struct TreeNode * root = newNode(nums[half]);
-    int i = 0;
-    for (i = 0; i < half;i++) {
-        
-    }
-    
+struct TNode* sortedToBST(int arr[], int start, int end)
+{
+    /* Base Case */
+    if (start > end)
+      return NULL;
+ 
+    /* Get the middle element and make it root */
+    int mid = (start + end)/2;
+    struct TreeNode *root = newNode(arr[mid]);
+ 
+    /* Recursively construct the left subtree and make it
+       left child of root */
+    root->left =  sortedToBST(arr, start, mid-1);
+ 
+    /* Recursively construct the right subtree and make it
+       right child of root */
+    root->right = sortedToBST(arr, mid+1, end);
+ 
+    return root;
 }
+ struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
+     struct TreeNode *root = sortedToBST(nums, 0, numsSize-1);
+     //int * ans = (int *)malloc(sizeof(int)*numsSize);
+   // preOrder(root,ans);
+     return root;
+ }
