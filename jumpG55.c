0e8b28ad4269cad2157df53f6336790a0c65080e
benchin <ben.chin@quantatw.com>
2018-08-25 23:36:36 +0800

    not the fatest method but it works!

--- jumpG55.c
+++ jumpG55.c
@@ -3,30 +3,20 @@
 #include<stdbool.h>
 bool jump(int* nums, int pos ,int  prevpos , int end) {
     int step = nums[pos];
-		printf("0. step is %d \n", step);
+                //printf("0. step is %d \n", step);
     if (pos + step >= end) {
         return true;
-    } else if (step > 0) {
-		prevpos = pos;
-        pos+=step;
-		printf("1. pos is %d and its step is %d \n", pos , nums[pos]);
-        if ( jump(nums,pos,prevpos ,end) ) {
+    } 
+        prevpos = pos;
+        //pos+=step;
+    for (int i = step; i > 0; i--) {
+        //printf("pos is %d ,   prevpos is %d   , step is %d \n", pos , prevpos , step);
+        if ( jump(nums,pos+i,prevpos,end) ) {
             return true;
         }
-		printf("2. prev pos is %d \n", prevpos);
-		if (nums[prevpos]-1 > 0) {
-			nums[prevpos]--;
-			pos = prevpos + nums[prevpos] ;
-            if (jump(nums,pos,prevpos,end) ) {
-				return true;
-			}
-        } else {
-            return false;
-        }
-        
     }
-   return false; 
     
+   return false;
 }
 bool canJump(int* nums, int numsSize) {
     if (numsSize == 1 && nums[0] == 0) {
@@ -42,4 +32,40 @@ int main() {
 		printf(" no , it can't ! \n" );
 	}
 	return 0;
+}
+//fatest method without using recursive
+bool canJump(int* a, int n) {
+    if(n==1)
+        return true;
+    if(a[0]==0)
+        return false;
+    int h = n-2;
+    while(h>=0)
+    {
+    while(h>0)
+    {
+        if(a[h]>0)
+            h--;
+        else
+            break;
+    }
+    if(h==0)
+        return true;
+    int k = 2;
+    h=h-1;
+    bool skip=false;
+    while(h>=0)
+    {
+        if(a[h]>=k)
+        {
+            skip=true;
+            break;
+        }
+        h--;
+        k++;
+    }
+    if(skip==false)
+        return false;
+    }
+    return false;
 }
