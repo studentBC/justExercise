32b40746711cad4e3e9c9e374434db65e010103c
ben.chin <ben.chin@quantatw.com>
2019-09-14 10:38:38 +0800

    a math problem, beat 67.69%
    we should try to get math equation before we solve this problem

---
+++ minimumMovesToEqualArrayElements453.cpp
@@ -0,0 +1,52 @@
+//concept:
+/*let's define sum as the sum of all the numbers, before any moves; minNum as the min number int the list; n is the length of the list;
+
+After, say m moves, we get all the numbers as x , and we will get the following equation
+
+ sum + m * (n - 1) = x * n
+and actually,
+
+  x = minNum + m
+This part may be a little confusing, but @shijungg explained very well. let me explain a little again. it comes from two observations:
+
+the minum number will always be minum until it reachs the final number, because every move, other numbers (besides the max) will be increamented too;
+from above, we can get, the minum number will be incremented in every move. So, if the final number is x, it would be minNum + moves;
+and finally, we will get
+
+  sum - minNum * n = m
+  */
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        long sum = 0, small = INT_MAX;
+        for (long i :nums) {
+            small = min(small, i);
+            sum+=i;
+        }
+        return sum-small*nums.size();
+    }
+};
+//the fatest solution
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int mini = INT_MAX;
+        int count = 0;
+        if(nums.size()==1){
+            return 0;
+        }
+        for(int i=0;i<nums.size();i++){
+            mini = min(mini, nums[i]);
+        }
+        for(int i=0;i<nums.size();i++){
+            count += (nums[i]-mini);
+        }
+        return count;
+    }
+};
+
+static int fast_io = []() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
