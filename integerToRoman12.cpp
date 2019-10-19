42f1dd879a3c157d2990795e55636b6c98baffd0
ben.chin <ben.chin@quantatw.com>
2019-10-19 10:13:02 +0800

    beat 50.15%, we should learn others method, pretty efficient

---
+++ integerToRoman12.cpp
@@ -0,0 +1,133 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int>number;
+        int temp = num, end;
+        while (temp) {
+            number.emplace_back(temp%10);
+            temp/=10;
+        }
+        //reverse(number.begin(), number.end());
+        vector<string>tmp;
+        string ans;
+        if (number.size() > 0) {
+            if (number[0] == 4) {
+                tmp.emplace_back("IV");
+            } else if (number[0] == 9) {
+                tmp.emplace_back("IX");
+            } else if (number[0] == 5) {
+                tmp.emplace_back("V");
+            } else {
+                string num;
+                if (number[0] < 5) {
+                    for (int i = 0; i < number[0]; i++) num+="I";
+                } else {
+                    num="V";
+                    end = number[0]-5;
+                    for (int i = 0; i < end; i++) num+="I";
+                }
+                tmp.emplace_back(num);
+            }
+        }
+        //tenth
+        if (number.size() > 1) {
+            if (number[1] == 4) {
+                tmp.emplace_back("XL");
+            } else if (number[1] == 9) {
+                tmp.emplace_back("XC");
+            } else if (number[1] == 5) {
+                tmp.emplace_back("L");
+            } else {
+                string num;
+                if (number[1] < 5) {
+                    for (int i = 0; i < number[1]; i++) num+="X";
+                } else {
+                    num="L";
+                    end = number[1]-5;
+                    for (int i = 0; i < end; i++) num+="X";
+                }
+                tmp.emplace_back(num);
+            }
+        }
+        //hundre
+        if (number.size() > 2) {
+            if (number[2] == 4) {
+                tmp.emplace_back("CD");
+            } else if (number[2] == 9) {
+                tmp.emplace_back("CM");
+            } else if (number[2] == 5) {
+                tmp.emplace_back("D");
+            } else {
+                string num;
+                if (number[2] < 5) {
+                    for (int i = 0; i < number[2]; i++) num+="C";
+                } else {
+                    num="D";
+                    end = number[2]-5;
+                    for (int i = 0; i < end; i++) num+="C";
+                }
+                tmp.emplace_back(num);
+            }
+        }
+        //thousand
+        if (number.size() > 3) {
+            string num;
+            for (int i = 0; i < number[3]; i++) num+="M";
+            tmp.emplace_back(num);
+        }
+        reverse(tmp.begin(), tmp.end());
+        for (string s:tmp) ans+=s;
+        return ans;
+    }
+};
+//the fatest solution
+class Solution {
+public:
+    string intToRoman(int num) {
+        string res;
+        while( num ) {
+            if( num >= 1000 ) {
+                res += "M";
+                num -= 1000;
+            } else if ( num >= 900 ) {
+                res += "CM";
+                num -= 900;
+            } else if ( num >= 500 ) {
+                res += "D";
+                num -= 500;
+            } else if ( num >= 400 ) {
+                res += "CD";
+                num -= 400;
+            } else if ( num >= 100 ) {
+                res += "C";
+                num -= 100;
+            } else if ( num >= 90 ) {
+                res += "XC";
+                num -= 90;                
+            } else if ( num >= 50 ) {
+                res += "L";
+                num -= 50;   
+            } else if ( num >= 40 ) {
+                res += "XL";
+                num -= 40;                          
+            } else if ( num >= 10 ) {
+                res += "X";
+                num -= 10;     
+            } else if ( num >= 9 ) {
+                res += "IX";
+                num -= 9;                     
+            } else if ( num >= 5 ) {
+                res += "V";
+                num -= 5;                     
+            } else if ( num >= 4 ) {
+                res += "IV";
+                num -= 4;                     
+            } else if ( num >= 1 ) {
+                res += "I";
+                num -= 1;                   
+            }            
+        }
+        return res;
+        
+    }
+};
