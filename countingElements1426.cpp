40f617bc88142feb60b993e5a7666ee248eaaf7e
benchin <ben.chin@quantatw.com>
2020-05-05 08:54:14 +0800

    locked problem, beat 60.3%

---
+++ countingElements1426.cpp
@@ -0,0 +1,65 @@
+/*
+Given an integer array arr, count how many elements x there are, such that x + 1 is also in arr.
+
+If there're duplicates in arr, count them seperately.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,3]
+Output: 2
+Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
+Example 2:
+
+Input: arr = [1,1,3,3,5,5,7,7]
+Output: 0
+Explanation: No numbers are counted, cause there's no 2, 4, 6, or 8 in arr.
+Example 3:
+
+Input: arr = [1,3,2,3,5,0]
+Output: 3
+Explanation: 0, 1 and 2 are counted cause 1, 2 and 3 are in arr.
+Example 4:
+
+Input: arr = [1,1,2,2]
+Output: 2
+Explanation: Two 1s are counted cause 2 is in arr.
+Example 5:
+
+Input: arr = [1,1,2]
+Output: 2
+Explanation: Both 1s are counted because 2 is in the array.
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+0 <= arr[i] <= 1000
+*/
+class Solution {
+public:
+    int countElements(vector<int>& arr) {
+ 		unordered_set<int>num;
+		int ans = 0;
+		for (int i:arr) num.insert(i);
+		for (int i:arr) {
+			if (num.count(i+1)) ans++;
+		}
+		return ans;
+    }
+};
+//the fatest solution
+class Solution {
+public:
+    int countElements(vector<int>& arr) {
+        int p[1002]={0};
+        int c=0;
+        for(auto x:arr){
+            p[x]=1;
+        }
+        for(auto x:arr)
+            c+=p[x+1]==1;
+        return c;
+    }
+};
