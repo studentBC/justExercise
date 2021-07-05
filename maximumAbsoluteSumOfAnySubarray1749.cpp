06f43860051f3c0eeacee2ae911d98f57e9b3679
ben.chin <ben.chin@quantatw.com>
2021-07-05 09:57:18 +0800

    beat 92% after knowing how to solve by kadane's algs
    we should review it again

--- maximumAbsoluteSumOfAnySubarray1749.cpp
+++ maximumAbsoluteSumOfAnySubarray1749.cpp
@@ -1,15 +1,18 @@
+//using kadane's algs to solve
+//https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        //vector<int>sum(nums.size(), 0);
-        int small = 0, s = 0, big = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            s+=nums[i];
-            //sum[i] = s;
-            small = min(s, small);
-            big = max(s, big);
+        int s = 0, ans = 0, len = nums.size(), big, small;
+        vector<int>localMax(len, INT_MIN);
+        vector<int>localMin(len, INT_MAX);
+        big = small = localMax[0] = localMin[0] = nums[0];
+        for (int i = 1; i < len; i++) {
+            localMax[i] = max(nums[i], nums[i]+localMax[i-1]);
+            big = max(big, localMax[i]);
+            localMin[i] = min(nums[i], nums[i]+localMin[i-1]);
+            small = min(small, localMin[i]);
         }
-        
-        return abs(big-small);
+        return max(abs(big), abs(small));
     }
 };
