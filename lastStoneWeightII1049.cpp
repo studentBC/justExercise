5cb52e4682524fb46d967af7f9390feb129c6cdc
studentBC <r04546032@ntu.edu.tw>
2021-07-29 14:06:39 +0800

    knapbacks problem, need to practice it again

--- lastStoneWeightII1049.cpp
+++ lastStoneWeightII1049.cpp
@@ -1,55 +1,62 @@
 /*
- *We have a collection of rocks, each rock has a positive integer weight.
+This question eaquals to partition an array into 2 subsets whose difference is minimal
+(1) S1 + S2  = S
+(2) S1 - S2 = diff  
 
-Each turn, we choose any two rocks and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+==> -> diff = S - 2 * S2  ==> minimize diff equals to  maximize S2 
 
-If x == y, both stones are totally destroyed;
-If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
-At the end, there is at most 1 stone left.  Return the smallest possible weight of this stone (the weight is 0 if there are no stones left.)
+Now we should find the maximum of S2 , range from 0 to S / 2, using dp can solve this
 
- 
+dp[i][j]   = {true if some subset from 1st to j'th has a sum equal to sum i, false otherwise}
+    i ranges from (sum of all elements) {1..n}
+    j ranges from  {1..n}
 
-Example 1:
 
-Input: [2,7,4,1,8,1]
-Output: 1
-Explanation: 
-We can combine 2 and 4 to get 2 so the array converts to [2,7,1,8,1] then,
-we can combine 7 and 8 to get 1 so the array converts to [2,1,1,1] then,
-we can combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
-we can combine 1 and 1 to get 0 so the array converts to [1] then that's the optimal value.
-[1,2,3,4,5,6,7,8,2,2,3,4,5,6,7,8,3,2,3,4,5,6,7,8,4,2,3,4,5,6,8,7] 
-[35,33,30,25,19,11,53,40,36,10,31,23,26,13,14,18,33,22,16,22,16,28,16,72,25,23,19]
-[58,60,49,53,22,6,21,15,39,63,47,7,26,24,54,59,69,60,19,39,28,47,25,62,15,25,62]
-Note:
-
-1 <= stones.length <= 30
-1 <= stones[i] <= 100
-
-   Hide Hint 1
-Think of the final answer as a sum of weights with + or - sign symbols infront of each weight. Actually, all sums with 1 of each sign symbol are possible.
-   Hide Hint 2
-Use dynamic programming: for every possible sum with N stones, those sums +x or -x is possible with N+1 stones, where x is the value of the newest stone. (This overcounts sums that are all positive or all negative, but those don't matter.)
- *
+One way to understand the fact that "The minimum result of cancellation = the minimum difference between the sum of two groups".
+Say you've already found two groups with smallest difference.
+Group A = [A1, A2, ..., An]
+Group B = [B1, B2, ..., Bm]
+The process we cancel two stones is to arbitrarily pick one from group A and one from Group B.
+If Ai > Bj, then put Ai-Bj into group A.
+If Ai < Bj, then put Ai-Bj into group B.
+If Ai = Bj, then nothing will be put into group A and B.
+We repeat the process until there is only one stone left. You will find the remaining stone is |sum(Group A) - sum(Group B)|.
  */
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
-        //cout << stones.size() <<" , " << stones[15];
-       vector<int> num;
-        int temp, ans = INT_MAX;
-        num.emplace_back(stones[0]);
-        for (int i = 1; i < stones.size(); ++i) {
-            vector<int> next;
-            while (!num.empty()) {
-                next.emplace_back(num.back()+stones[i]);
-                next.emplace_back(num.back()-stones[i]);
-                num.pop_back();
+	int sum = 0, ans = 0, len = stones.size(), half = len/2;
+	for (int i : stones) sum+=i;
+       vector<vector<bool>>dp(sum+1, vector<bool>(len+1, false));
+       for (int i = 0; i <= len; i++) dp[0][i] = true;
+        for (int i = 1; i <= len; ++i) {
+            for (int s = 1; s <= half; s++) {
+		if (dp[s][i-1] || (s >= stones[i-1] && dp[s-stones[i-1]][i-1])) {
+			dp[s][i] = true;
+			ans = max(ans, s);
+		}
             }
-            num = next;
         }
-        for (int i:num) ans = min(ans, abs(i));
-        return ans;
+        return sum-2*ans;
+    }
+};
+
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int half, sum = 0, len = stones.size();
+        for (int i : stones) sum+=i;
+        half = sum/2;
+        vector<vector<int>>dp(len+1, vector<int>(half+1, 0));
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j <= half; j++) {
+                if (j >= stones[i-1]) {
+                    dp[i][j] = max(dp[i-1][j], stones[i-1]+dp[i-1][j-stones[i-1]]);
+                } else dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return sum-dp.back().back()*2;
     }
 };
 /*the fatest method
