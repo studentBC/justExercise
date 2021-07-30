d1fc2c16f160a7ca834e07b652f72be09585f62c
ben.chin <ben.chin@quantatw.com>
2021-07-30 21:56:40 +0800

    very nice math like dp problem
    we should review it again before having interview

---
+++ uniqueBinarySearchTrees96.cpp
@@ -0,0 +1,16 @@
+/*
+https://leetcode.com/problems/unique-binary-search-trees/discuss/31666/DP-Solution-in-6-lines-with-explanation.-F(i-n)-G(i-1)-*-G(n-i)
+*/
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int>dp(n+1, 0);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j-1]*dp[i-j];
+            }
+        }
+        return dp.back();
+    }
+};
