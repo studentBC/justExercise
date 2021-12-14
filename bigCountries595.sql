682b33553f9b1f2b713bfbff30a79e4d1757bc5e
studentBC <r04546032@ntu.edu.tw>
2021-12-14 11:22:27 -0800

    easy and fundamental sql

---
+++ bigCountries595.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select name, population, area
+from World
+where population >= 25000000 or area >= 3000000;
+
+# Write your MySQL query statement below
+select name,population,area from World where area>=3000000 or population>=25000000;
