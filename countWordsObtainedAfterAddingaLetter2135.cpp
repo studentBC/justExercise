67de4cdf3539924785086774648009e81a28d56a
studentBC <r04546032@ntu.edu.tw>
2022-01-09 12:13:51 -0800

    revise and need to practice

--- countWordsObtainedAfterAddingaLetter2135.cpp
+++ countWordsObtainedAfterAddingaLetter2135.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int convert (string s) {
-            int ans = 0;
-            for (char c : s) ans+=((c-'a'));
-            return ans;
-    }
-    int wordCount(vector<string>& startWords, vector<string>& targetWords) {
-        unordered_set<int>number;
-        for (string& s : startWords) number.insert(convert(s));
-        int ans = 0, num, temp;
-        for (string s: targetWords) {
-            num = convert(s);
-            for (int i = 0; i < 27; i++) {
-                if ((num & (i)) > 0) {
-                    temp = num-(i);
-                    if (number.count(temp)) {ans++; break;}
-                }
-            }
+        int convert (string s) {
+                int ans = 0;
+                for (char c : s) ans+=(1<<(c-'a'));
+                return ans;
         }
-        return ans;
+    int wordCount(vector<string>& startWords, vector<string>& targetWords) {
+                unordered_set<int>number;
+                for (string& s : startWords) number.insert(convert(s));
+                int ans = 0, num, temp;
+                for (string s: targetWords) {
+                        num = convert(s);
+                        for (int i = 0; i < 27; i++) {
+                                if ((num & (1<<i)) > 0) {
+                                        temp = num-(1<<i);
+                                        if (number.count(temp)) {ans++; break;}
+                                }
+                        }
+                }
+                return ans;
     }
 };
+
+
