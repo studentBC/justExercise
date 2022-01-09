2bcdcf28eb0bf2dcbdd9084f6d51311dc79a941d
studentBC <r04546032@ntu.edu.tw>
2022-01-09 13:33:44 -0800

    beat 15%

---
+++ actorsAndDirectorsWhoCooperatedAtLeastThreeTimes1050.sql
@@ -0,0 +1,63 @@
+Table: ActorDirector
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| actor_id    | int     |
+| director_id | int     |
+| timestamp   | int     |
++-------------+---------+
+timestamp is the primary key column for this table.
+ 
+
+Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+ActorDirector table:
++-------------+-------------+-------------+
+| actor_id    | director_id | timestamp   |
++-------------+-------------+-------------+
+| 1           | 1           | 0           |
+| 1           | 1           | 1           |
+| 1           | 1           | 2           |
+| 1           | 2           | 3           |
+| 1           | 2           | 4           |
+| 2           | 1           | 5           |
+| 2           | 1           | 6           |
++-------------+-------------+-------------+
+Output: 
++-------------+-------------+
+| actor_id    | director_id |
++-------------+-------------+
+| 1           | 1           |
++-------------+-------------+
+Explanation: The only pair is (1, 1) where they cooperated exactly 3 times.
+
+
+# Write your MySQL query statement below
+select actor_id, director_id
+from  (
+    select actor_id, director_id, count(*) as c
+    from ActorDirector
+    group by director_id, actor_id
+) as temp
+where temp.c > 2
+
+
+# 266 ms solution 
+with cte as(
+select actor_id,director_id,count(director_id) as cnt from ActorDirector
+group by actor_id,director_id)
+
+
+select actor_id,director_id
+from cte
+where cnt>=3
