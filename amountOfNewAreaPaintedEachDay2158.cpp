4c8e4be15500ba4f7e302fc795f0e415c3eb46cb
studentBC <r04546032@ntu.edu.tw>
2022-08-20 19:27:31 -0700

    add merge solution

--- amountOfNewAreaPaintedEachDay2158.cpp
+++ amountOfNewAreaPaintedEachDay2158.cpp
@@ -61,3 +61,28 @@ public:
         return ans;
     }
 };
+
+//interval merge solution
+vector<int> amountPainted(vector<vector<int>>& pt) {
+    map<int, int> m;
+    vector<int> res;
+    for (auto &p : pt) {
+        int l = p[0], r = p[1];
+        auto next = m.upper_bound(l), cur = next;
+        if (cur != begin(m) && prev(cur)->second >= l) {
+            cur = prev(cur);
+            l = cur->second;
+        }
+        else 
+            cur = m.insert({l, r}).first;
+        int paint = r - l;
+        while (next != end(m) && next->first < r) {
+            paint -= min(r, next->second) - next->first;
+            r = max(r, next->second);
+            m.erase(next++);
+        }
+        cur->second = max(cur->second, r);
+        res.push_back(max(0, paint));
+    }
+    return res;
+}
