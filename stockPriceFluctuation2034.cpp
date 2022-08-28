ec9c59a20fb8d4e895f703268f77e9256d4cb0a9
studentBC <r04546032@ntu.edu.tw>
2022-08-28 16:05:08 -0700

    come out with my own solution and beat 55%
    interview question for G

--- stockPriceFluctuation2034.cpp
+++ stockPriceFluctuation2034.cpp
@@ -1,5 +1,61 @@
 class StockPrice {
 public:
+    int cur = -1, p = -1;
+    map<int, unordered_set<int>>pt; //price timestamp
+    unordered_map<int, int>tp;// timestamp price
+    StockPrice() {
+        
+    }
+    
+    void update(int timestamp, int price) {
+        if (cur <= timestamp) {
+            cur = timestamp;
+            p = price;
+        } 
+        
+        if (tp.count(timestamp) && pt.count(tp[timestamp])  && pt[tp[timestamp]].count(timestamp)) {
+            //the bug is that we still need to keep every legal price and timestamp so that we can get the correct value
+            pt[tp[timestamp]].erase(timestamp);
+            if (pt[tp[timestamp]].empty()) pt.erase(tp[timestamp]);
+            pt[price].insert(timestamp);
+        }
+        tp[timestamp] = price;
+        
+        pt[price].insert(timestamp);
+        
+    }
+    
+    int current() {
+        return p;
+    }
+    
+    int maximum() {
+        for (auto it = pt.rbegin(); it != pt.rend(); it++) {
+            
+            return it->first;
+        }
+        return 0;
+    }
+    
+    int minimum() {
+        for (auto& it : pt) {
+            //cout << it.second <<", ";
+            return it.first;
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice* obj = new StockPrice();
+ * obj->update(timestamp,price);
+ * int param_2 = obj->current();
+ * int param_3 = obj->maximum();
+ * int param_4 = obj->minimum();
+ */
+class StockPrice {
+public:
 	map<int,int>sp;
 	multiset<int>p;
     StockPrice() {
