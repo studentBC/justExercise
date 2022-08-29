1e7e0f6e6c0aa3e309e35e4126381eab2227f777
studentBC <r04546032@ntu.edu.tw>
2022-08-28 17:44:39 -0700

    kahn's  topological sort
    need to review concept before doing interview with G

---
+++ findAllPossibleRecipesFromGivenSupplies2115.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_map<string, vector<string>>graph;
+        unordered_map<string, int> indegree;
+        unordered_set<string>sup(supplies.begin(), supplies.end());
+        int len = ingredients.size();
+        for (string& s : recipes) indegree[s] = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < ingredients[i].size(); j++) {
+                if (!sup.count(ingredients[i][j])) {
+                    graph[ingredients[i][j]].push_back(recipes[i]);
+                    indegree[recipes[i]]++;
+                }
+            }
+        }
+        queue<string>next;
+        vector<string>ans;
+        for (auto& it: indegree) {
+            if (it.second == 0) {
+                next.push(it.first);
+                ans.push_back(it.first);
+            }
+        }
+        string n;
+        
+        while (!next.empty()) {
+            n = next.front();
+            for (string& s: graph[n]) {
+                indegree[s]--;
+                if (indegree[s] == 0) {
+                    next.push(s);
+                    ans.push_back(s);
+                }
+            }
+            next.pop();
+        }
+        return ans;
+    }
+};
