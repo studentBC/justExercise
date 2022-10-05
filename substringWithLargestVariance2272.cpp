5f145fd0107b078a90d1fc1684a4edf76c95a67f
studentBC <r04546032@ntu.edu.tw>
2022-10-04 19:08:24 -0700

    need to reveiew it again

--- substringWithLargestVariance2272.cpp
+++ substringWithLargestVariance2272.cpp
@@ -1,3 +1,37 @@
+//https://www.youtube.com/watch?v=DbfHIdZL8rQ&ab_channel=CodeWithSunny
+class Solution {
+public:
+    int largestVariance(string s) {
+        int ans = 0, ca, cb, count;
+        int freq[26] = {0};
+        for (char c : s) freq[c-'a']++;
+        unordered_set<char>alpha(begin(s), end(s));
+        for (char a : alpha) {
+            for (char b: alpha) {
+                if (a == b) continue;
+                //count = freq[a-'a'];
+                for (int i = 0; i < 2; i++) {
+                    
+                    ca = cb = 0;
+                    for (char c : s) {
+                        if (c == b) cb++;
+                        if (c == a) {
+                            ca++;
+                        }
+                        //cout << cb <<", " << ca << endl;
+                        if (cb < ca) cb = ca = 0;
+                        if (ca && cb && cb > ca) ans = max(ans, cb-ca);
+                    }
+                    reverse(s.begin(), s.end());
+					//we do the reverse for the corner cases like
+					//abbbbb
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
 //Kadanes algorithm
 class Solution {
 public:
