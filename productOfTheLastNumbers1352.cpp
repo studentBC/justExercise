bb973d7ea8ab88d7f7bdeccda290bc29bafe4953
studentBC <r04546032@ntu.edu.tw>
2022-10-13 23:33:49 -0700

    update solution

--- productOfTheLastNumbers1352.cpp
+++ productOfTheLastNumbers1352.cpp
@@ -1,3 +1,47 @@
+//my solution at 2022/10/13
+class ProductOfNumbers {
+public:
+    vector<int>number;
+    vector<pair<long long, int>>product;
+    long long n = 1;
+    int len = 0, start = INT_MAX;
+    ProductOfNumbers() {
+        
+    }
+    
+    void add(int num) {
+        number.push_back(num);
+        n*=num;
+        if (n == 0) {
+            n = 1;
+            start = INT_MAX;
+        } else {
+            if (start == INT_MAX) start = len;
+        }
+        product.push_back({n, start});
+        len++;
+    }
+    
+    int getProduct(int k) {
+        //cout << product.back().second << endl;
+        if (product.back().second <= len-k) {
+            //cout << k << endl;
+            if (product.back().second == len-k) return product.back().first;
+            else {
+                //cout <<product.back().second;
+                return product.back().first/product[len-k-1].first;
+            }
+        }
+        return 0;
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
 class ProductOfNumbers {
 public:
     ProductOfNumbers() {
