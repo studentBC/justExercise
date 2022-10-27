b1af8bb82589621a2806e7e0fb85b84cfdaa5dd5
studentBC <r04546032@ntu.edu.tw>
2022-10-26 22:57:43 -0700

    need to practice it again

--- theWinnerUniversity2072.sql
+++ theWinnerUniversity2072.sql
@@ -6,4 +6,20 @@ select
     ELSE 'No Winner'
 END) as winner
 
+# Write your MySQL query statement below
+select 
+case
+    when (select count(student_id) from NewYork where score > 89) > (select count(student_id) from California where score > 89) then 'New York University'
+    when (select count(student_id) from NewYork where score > 89) < (select count(student_id) from California where score > 89) then 'California University'
+    else 'No Winner'
+end
+as winner
 
+
+
+
+# SELECT (CASE 
+#             WHEN (SELECT COUNT(score) FROM NewYork WHERE score >= 90) > (SELECT COUNT(score) FROM California WHERE score >= 90) THEN 'New York University'
+#             WHEN (SELECT COUNT(score) FROM NewYork WHERE score >= 90) < (SELECT COUNT(score) FROM California WHERE score >= 90) THEN 'California University'
+#             ELSE 'No Winner' END) AS WINNER
+# ;
