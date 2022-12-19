4a85dbf80021b4a3b961d70db9c8e9ca652cdc6a
studentBC <r04546032@ntu.edu.tw>
2022-12-19 21:19:29 +0800

    beat 22%

--- removeInterval1272.cpp
+++ removeInterval1272.cpp
@@ -1,3 +1,52 @@
+/*
+ * A set of real numbers can be represented as the union of several disjoint intervals, where each interval is in the form [a, b). A real number x is in the set if one of its intervals [a, b) contains x (i.e. a <= x < b).
+
+You are given a sorted list of disjoint intervals intervals representing a set of real numbers as described above, where intervals[i] = [ai, bi] represents the interval [ai, bi). You are also given another interval toBeRemoved.
+
+Return the set of real numbers with the interval toBeRemoved removed from intervals. In other words, return the set of real numbers such that every x in the set is in intervals but not in toBeRemoved. Your answer should be a sorted list of disjoint intervals as described above.
+
+Input: intervals = [[0,2],[3,4],[5,7]], toBeRemoved = [1,6]
+Output: [[0,1],[6,7]]
+
+Input: intervals = [[0,5]], toBeRemoved = [2,3]
+Output: [[0,2],[3,5]]
+
+Input: intervals = [[-5,-4],[-3,-2],[1,2],[3,5],[8,9]], toBeRemoved = [-1,4]
+Output: [[-5,-4],[-3,-2],[4,5],[8,9]]
+ */
+
+class Solution {
+public:
+    vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
+        vector<vector<int>>ans;
+        int start, end, ps = -1, pe = -1;
+        for (int i = 0; i < intervals.size(); i++) {
+            if (intervals[i][0] > toBeRemoved[1] || intervals[i][1] < toBeRemoved[0]) ans.push_back(intervals[i]);
+            else {
+                if (intervals[i][0] < toBeRemoved[0]) {
+                    start = intervals[i][0];
+                    end = toBeRemoved[0];
+                    if (ps != start && pe != end) {
+                        ans.push_back({start, end});
+                        ps = start; pe = end;
+                    }
+                }
+                
+                if (intervals[i][1] > toBeRemoved[1]) {
+                    start = toBeRemoved[1];
+                    end = intervals[i][1];
+                    if (ps != start && pe != end) {
+                        ans.push_back({start, end});
+                        ps = start; pe = end;
+                    }
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
+
 class Solution {
 public:
     vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
