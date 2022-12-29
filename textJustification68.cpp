d305dfa133ee7ffe8a1b3f6c500444de95b4cd32
studentBC <r04546032@ntu.edu.tw>
2022-12-29 22:04:39 +0800

    update my solution of beat 14%

--- textJustification68.cpp
+++ textJustification68.cpp
@@ -1,6 +1,76 @@
 class Solution {
 public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string>ans;
+        vector<int>len;
+        int left = maxWidth, length = words.size(), width, space, i = 0, sum, end;
+        for (string& s: words) len.push_back(s.size());
+        while (i < length) {
+            width = maxWidth;
+            vector<int> tmp;
+            for (int j = i; j < length && width >= len[j]; j++) {
+                tmp.push_back(j);
+                width-=len[j];
+                width--;
+            }
+            i = tmp.back()+1;
+            sum = 0;
+            for (int a: tmp) sum+=len[a];
+            
+            string temp, sp;
+            if (i == length) {
+                end = tmp.size()-1;
+                space = maxWidth-sum-(tmp.size()-1);
+                for (int j = 0; j < end; j++) {
+                    temp+=words[tmp[j]];
+                    temp.push_back(' ');
+                }
+                temp+=words[tmp[end]];
+                for (int j = 0; j < space; j++) temp.push_back(' ');
+            } else {
+                if (tmp.size() == 1) {
+                    space = maxWidth-sum;
+                    temp+=words[tmp[0]];
+                    for (int a = 0; a < space; a++) temp.push_back(' ');
+                    ans.push_back(temp);
+                    continue;
+                }
+                //cout << tmp.size() << endl;
+                space = (maxWidth-sum)/(tmp.size()-1);
+                for (int a = 0; a < space; a++) sp.push_back(' ');
+                int redundant = (maxWidth-sum)%(tmp.size()-1);
+                if (redundant) {
+                    sp.push_back(' ');
+                    //cout << sp.size() << endl;
+                    end = tmp.size();
+                    int j = 0;
+                    for (j = 0; j < end && j < redundant; j++) {
+                        temp+=words[tmp[j]];
+                        temp+=sp;
+                    }
+                    sp.pop_back();
+                    for ( ; j < end-1; j++) {
+                        temp+=words[tmp[j]];
+                        temp+=sp;
+                    }
+                    temp+=words[tmp.back()];
+                } else {
+                    for (int j = 0; j < tmp.size()-1; j++) {
+                        temp+=words[tmp[j]];
+                        temp+=sp;
+                    }
+                    temp+=words[tmp.back()];
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
         vector<string>ans, line;
         int index = 0, left = maxWidth, end = words.size()-1;
         string temp;
